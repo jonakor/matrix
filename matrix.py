e1e549ed3464ee8b3abe2b87a90d3ff72edc2df7
jonakor <jon18andreas@gmail.com>
2020-04-30 00:01:14 +0200

    please

--- matrix.py
+++ matrix.py
@@ -221,7 +221,7 @@ def pulseApp():
 def displayText():
     global strip, intext, textFlag
     if not textFlag:
-	return
+	   return
     roll = text.text_to_roll(inText)
     index = 0
     image = [[40,41,42,43,44,45,46,47,48,49],[30,31,32,33,34,35,36,37,38,39],[20,21,22,23,24,25,26,27,28,29],[10,11,12,13,14,15,16,17,18,19],[0,1,2,3,4,5,6,7,8,9]]
@@ -238,9 +238,9 @@ def displayText():
                     done = False
                     strip.setPixelColor(image[i][j], Color(int(blue), int(red), int(green)))
 
-    index += 1
-	strip.show()
-	time.sleep(0.5)
+        index += 1
+        strip.show()
+        time.sleep(0.5)
         if done:
             index = 0
 
